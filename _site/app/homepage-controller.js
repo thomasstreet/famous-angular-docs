c09f8ba9bac203f13f728cb7f04479ece7c8d8e0
Andre Levi <andrelevi09@gmail.com>
2014-05-15 11:21:53 -0700

    Add example famous-angular code for homepage.

--- _site/app/homepage-controller.js
+++ _site/app/homepage-controller.js
@@ -1,5 +1,26 @@
 angular.module('controller.homepage', [])
-  .controller('homepageCtrl', function() {
-    
+  .controller('homepageExamplesCtrl', function($scope) {
+
+    $scope.testArray = [
+      100, 200, 250, 300,
+      350, 450, 500, 600
+    ];
+    $scope.data = {
+      distance: 200
+    };
+
+    var _width = 320;
+    var _height = 568;
+
+    $scope.options = {
+      grid: {
+        dimensions: [4, 2]
+      }
+    }; 
+
+    $scope.sizes = {
+      gridSurfaces: [$scope.data.distance, undefined]
+    };
+
   })
 ;
