b7937b850c5b8b161535dd896381f02df7ac8170
Andre Levi <andrelevi09@gmail.com>
2014-05-17 17:58:52 -0700

    Use app.min.js for app scripts.

---
+++ _site/js/app.js
@@ -0,0 +1,20 @@
+
+angular.module('famous-angular', ['controller.homepage', 'famous.angular']);
+
+angular.module('controller.homepage', ['famous.angular'])
+  .controller('homepageExamplesCtrl', function($scope) {
+
+    $scope.options = {
+      grid: {
+        dimensions: [2, 2]
+      }
+    };
+
+    $scope.squares = [
+      { x: 0 }, { x: 100 }, { x: 200 }, { x: 300 }
+    ];
+
+    $scope.translateX = 50;
+
+  })
+;
