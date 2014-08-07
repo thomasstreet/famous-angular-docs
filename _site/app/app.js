ae1c616a98799d838fb22d17bffb8e7678303e88
Zack Brown <z@charybrown.com>
2014-08-06 18:28:18 -0700

    chore: update build

--- _site/app/app.js
+++ _site/app/app.js
@@ -1,3 +1,7 @@
+function translateSquare(index) {
+  return (index || 0) * 75;
+}
+
 angular.module('controller.homepage', ['famous.angular'])
   .controller('homepageExamplesCtrl', function($scope) {
 
@@ -7,13 +11,72 @@ angular.module('controller.homepage', ['famous.angular'])
       }
     };
 
-    $scope.squares = [
-      { x: 0 }, { x: 75 }, { x: 150 }, { x: 225 }
-    ];
+    $scope.squares = _.map(_.range(0, 4), function(index) {
+      return { x: translateSquare(index) };
+    });
 
-    $scope.translateX = 50;
+    $scope.repeatedItems = 1;
+    $scope.modifiers     = [0, 1, 2];
 
+    $scope.$watch('repeatedItems', function(value) {
+      $scope.modifiers = _.range(0, value);
+    });
   })
+  .directive('square', function($famous) {
+    var Transform = $famous['famous/core/Transform'];
+    var Transitionable = $famous['famous/transitions/Transitionable'];
+    var SnapTransition = $famous['famous/transitions/SnapTransition'];
+
+    var flipOut = Transform.rotateZ(Math.PI / 2);
+    var flipIn  = Transform.inverse(flipOut);
+
+    return {
+      restrict: 'A',
+      priority: 3,
+      link: function(scope, element, attrs) {
+        var transform = new Transitionable(Transform.multiply(
+          Transform.translate(translateSquare(scope.$index % 4), Math.floor(scope.$index / 4) * 60 + 40, 0),
+          flipOut
+        ));
+
+        var opacity = new Transitionable(0);
+
+        function enter() {
+          transform.set(Transform.multiply(transform.get(), flipIn), {
+            duration: 250,
+            transition: SnapTransition
+          });
+
+          opacity.set(1, {duration: 250, curve: 'easeOut'});
+
+          return 250;
+        };
+
+        function leave() {
+          transform.set(Transform.multiply(transform.get(), flipOut), {
+            duration: 250,
+            transition: SnapTransition
+          });
+
+          opacity.set(0, {duration: 250, curve: 'easeIn'});
+
+          return 250;
+        };
+
+        function halt() {
+          scope.transform.halt();
+        };
+
+        angular.extend(scope, {
+          enter: enter,
+          leave: leave,
+          halt: halt,
+          opacity: opacity,
+          transform: transform
+        });
+      }
+    }
+  });
 ;
 
 
