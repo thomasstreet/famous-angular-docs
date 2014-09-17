a465a301a89e57bb3dfdbd33c873ccae6765fdba
Andre Levi <andrelevi09@gmail.com>
2014-09-17 16:08:17 -0700

    move state styles into separate .styl files

--- _site/app/fa-app-classes.js
+++ _site/app/fa-app-classes.js
@@ -3,6 +3,7 @@ angular.module('famous-angular')
 .run(function($rootScope) {
 
   $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+
     // Make sure that fa-app contains the class for both the previous state
     // and current state, to ensure the correct CSS namespacing when
     // transitioning between views with fa-animate-enter/leave
