286b7bf5fcceac0d012107f1fffbbeb44788251e
Andre Levi <andrelevi09@gmail.com>
2014-09-19 15:40:35 -0700

    add basic fade animations for ng-repeat

--- app/state-3-ctrl.js
+++ app/state-3-ctrl.js
@@ -1,6 +1,6 @@
 angular.module('famous-angular')
 
-.controller('state3Ctrl', function($scope, $famous, $timeline, stateTransitions) {
+.controller('state3Ctrl', function($scope, $interval, $famous, $timeline, stateTransitions) {
 
   var Transitionable = $famous['famous/transitions/Transitionable'];
   var Easing = $famous['famous/transitions/Easing'];
@@ -45,10 +45,10 @@ angular.module('famous-angular')
   };
 
   $scope.data = {
-    repeatCount: 1
+    repeatCount: 0
   };
 
-  $scope.cats = [{}];
+  $scope.cats = [];
 
   $scope.$watch('data.repeatCount', function(newVal, oldVal) {
     if (newVal) {
@@ -60,54 +60,86 @@ angular.module('famous-angular')
   });
 
   $scope.catTile = {
-    translate: function($index) {
+    translate: function(catT, $index) {
+      var x = $index >= 4 ? 205 : 0;
+      var y = ($index % 4) * 80;
+      var z = 0;
       return $timeline([
-        [0.2, [0, 1000, 0], Easing.inOutQuart],
-        [0.3, [$index >= 4 ? 205 : 0, ($index % 4) * 80, 0]]
-      ])(t.get());
+        [0.2, [x >= 4 ? x + 60 : x - 60, y, z], Easing.inQuart],
+        [1, [x, y, z]]
+      ])(catT.get());
+    },
+    opacity: function(catT, $index) {
+      return catT.get();
     }
+  };
+
+  setTimeout(function() {
+    var repeatAutoplay = $interval(function() {
+      if ($scope.data.repeatCount + 1 >= 9) {
+        $interval.cancel(repeatAutoplay);
+        return;
+      }
+      $scope.data.repeatCount++;
+    }, 50);
+  }, 2000);
+
+  $scope.catEnter = function(t, $done) {
+    t.set(1, { duration: 300 }, $done);
+  };
+
+  $scope.catLeave = function(t, $done) {
+    t.set(0, { duration: 300 }, $done);
   };
 
   var catData = [
     {
       picture: 'img/cats/cat1.png',
       name: 'Rocco',
-      location: 'Seattle, WA'
+      location: 'Seattle, WA',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat2.png',
       name: 'Tabby',
-      location: 'Phoenix, AR'
+      location: 'Phoenix, AR',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat3.png',
       name: 'Meiska',
-      location: 'Reston, VA'
+      location: 'Reston, VA',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat4.png',
       name: 'Fat Max',
-      location: 'San Francisco, CA'
+      location: 'San Francisco, CA',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat5.png',
       name: 'Izzy',
-      location: 'Atlanta, GA'
+      location: 'Atlanta, GA',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat6.png',
       name: 'Powder',
-      location: 'Seattle, WA'
+      location: 'Seattle, WA',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat7.png',
       name: 'David',
-      location: 'Salem, OR'
+      location: 'Salem, OR',
+      t: new Transitionable(0)
     },
     {
       picture: 'img/cats/cat8.png',
       name: 'Maggie',
-      location: 'Orlando, FL'
+      location: 'Orlando, FL',
+      t: new Transitionable(0)
     }
   ];
 
