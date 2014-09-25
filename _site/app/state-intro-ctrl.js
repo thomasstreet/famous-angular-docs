337177a9958ebff39374ca59c72bb7e34923c20a
Andre Levi <andrelevi09@gmail.com>
2014-09-24 17:26:20 -0700

    add new stagger and rotation animation for intro state

--- _site/app/state-intro-ctrl.js
+++ _site/app/state-intro-ctrl.js
@@ -52,14 +52,14 @@ angular.module('famous-angular')
 
   $scope.logo = {
     translate: $timeline([
-      [0, [0, -200, 0], Easing.inOutQuart],
+      [0, [0, 200, 0], Easing.inOutQuart],
       [0.2, [0, 80, 0]],
       [1.6, [0, 80, 0], Easing.outQuad],
-      [2, [0, -200, 0]]
+      [2, [0, 200, 200]]
     ]),
     opacity: $timeline([
       [0, 0],
-      [0.3, 1, Easing.inCubic],
+      [0.2, 1, Easing.inCubic],
       [1.8, 1, Easing.inOutQuart],
       [2, 0]
     ])
@@ -68,14 +68,16 @@ angular.module('famous-angular')
 
   $scope.heading = {
     translate: $timeline([
-      [0, [0, -200, 0], Easing.inOutQuart],
-      [0.2, [0, 210, 0]],
+      [0, [0, 400, 0]],
+      [0.1, [0, 400, 0], Easing.inOutQuart],
+      [0.3, [0, 210, 0]],
       [1.6, [0, 210, 0], Easing.outQuad],
       [2, [0, -200, 0]]
     ]),
     opacity: $timeline([
       [0, 0],
-      [0.3, 1, Easing.inCubic],
+      [0.1, 0, Easing.inCubic],
+      [0.3, 1],
       [1.8, 1, Easing.inOutQuart],
       [2, 0]
     ])
@@ -83,14 +85,16 @@ angular.module('famous-angular')
 
   $scope.tagline = {
     translate: $timeline([
-      [0, [0, -200, 0], Easing.inOutQuart],
-      [0.2, [0, 350, 0]],
+      [0, [0, 500, 0]],
+      [0.2, [0, 500, 0], Easing.inOutQuart],
+      [0.4, [0, 350, 0]],
       [1.6, [0, 350, 0], Easing.outQuad],
       [2, [0, -200, 0]]
     ]),
     opacity: $timeline([
       [0, 0],
-      [0.3, 1, Easing.inCubic],
+      [0.2, 0],
+      [0.4, 1, Easing.inCubic],
       [1.8, 1, Easing.inOutQuart],
       [2, 0]
     ])
@@ -99,46 +103,55 @@ angular.module('famous-angular')
   $scope.buttons = {
     download: {
       translate: $timeline([
-        [0, [0, 490, 0], Easing.inOutQuart],
-        [0.2, [0, 490, 0], Easing.inOutQuart],
-        [0.4, [-300, 490, 0]],
+        [0, [0, 490, 0]],
+        [0.5, [0, 490, 0], Easing.inOutQuart],
+        [0.7, [-300, 490, 0]],
         [1.6, [-300, 490, 0], Easing.outQuad],
         [2, [0, -200, 500]]
       ]),
       opacity: $timeline([
         [0, 0],
-        [0.2, 0],
-        [0.4, 1, Easing.inCubic],
+        [0.5, 0],
+        [0.7, 1],
         [1.8, 1, Easing.inOutQuart],
         [2, 0]
       ])
     },
     github: {
+      rotateX: $timeline([
+        [0, -Math.PI / 2],
+        [0.3, -Math.PI / 2, Easing.outElastic],
+        [1, 0],
+        [1.5, 0, Easing.inOutQuart],
+        [2, Math.PI]
+      ]),
       translate: $timeline([
-        [0, [0, -200, 0], Easing.inOutQuart],
-        [0.3, [0, 490, 0]],
+        [0, [0, 490, 0]],
+        [0.3, [0, 490, 0], Easing.inOutQuart],
+        //[0.5, [0, 490, 0]],
         [1.6, [0, 490, 0], Easing.outQuad],
         [2, [0, -200, 500]]
       ]),
       opacity: $timeline([
         [0, 0],
-        [0.2, 1, Easing.inCubic],
+        [0.3, 0, Easing.inCubic],
+        [0.4, 1],
         [1.8, 1, Easing.inOutQuart],
         [2, 0]
       ])
     },
     docs: {
       translate: $timeline([
-        [0, [0, 490, 0], Easing.inOutQuart],
-        [0.2, [0, 490, 0], Easing.inOutQuart],
-        [0.4, [300, 490, 0]],
+        [0, [0, 490, 0]],
+        [0.5, [0, 490, 0], Easing.inOutQuart],
+        [0.7, [300, 490, 0]],
         [1.6, [300, 490, 0], Easing.outQuad],
         [2, [0, -200, 500]]
       ]),
       opacity: $timeline([
         [0, 0],
-        [0.2, 0],
-        [0.4, 1, Easing.inCubic],
+        [0.5, 0],
+        [0.7, 1, Easing.inCubic],
         [1.8, 1, Easing.inOutQuart],
         [2, 0]
       ])
