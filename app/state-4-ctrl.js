d0fd972cd1203c39245bd3a2815fc1cc8a3a661d
Andre Levi <andrelevi09@gmail.com>
2014-09-29 11:39:26 -0700

    set up animation for state 4

--- app/state-4-ctrl.js
+++ app/state-4-ctrl.js
@@ -36,12 +36,12 @@ angular.module('famous-angular')
 
   $scope.heading = {
     translate: $timeline([
-      [0, [0, -100, 0],Easing.outQuad],
-      [0.3, [0, 0, 0]]
+      [0, [0, 0, -150], Easing.outQuad],
+      [0.2, [0, 0, 0]]
     ]),
     opacity: $timeline([
       [0, 0, Easing.inQuart],
-      [0.3, 1]
+      [0.2, 1]
     ])
   };
 
@@ -49,22 +49,31 @@ angular.module('famous-angular')
   $scope.routing = {
    text: {
      translate: $timeline([
-       [0, [310, 630, 0], Easing.inOutQuart],
-       [0.3, [310, 630, 0]]
+       [0, [330, 830, 0], Easing.outBack],
+       [0.25, [330, 830, 0], Easing.outBack],
+       [0.55, [330, 680, 0]]
      ]),
      scale: $timeline([
        [0, [0.2, 0.2], Easing.outBack],
-       [0.3, [1, 1]]
+       [0.25, [0.2, 0.2], Easing.outBack],
+       [0.55, [1, 1]]
+     ]),
+     opacity: $timeline([
+       [0, 0],
+       [0.25, 0],
+       [0.55, 1]
      ])
    }, 
    image: {
      translate: $timeline([
-       [0, [180, 1050, 0], Easing.outBack],
-       [0.2, [180, 350, 0]]
+       [0, [140, 600, 0], Easing.outBack],
+       [0.2, [140, 600, 0], Easing.outBack],
+       [0.5, [140, 370, 0]]
      ]),
      opacity: $timeline([
        [0, 0, Easing.outCubic],
-       [0.3, 1]
+       [0.2, 0, Easing.outCubic],
+       [0.5, 1]
      ])
    },
   };
@@ -72,18 +81,31 @@ angular.module('famous-angular')
   $scope.dependency = {
    text: {
      translate: $timeline([
-       [0, [750, 630, 0], Easing.inOutQuart],
-       [0.2, [750, 630, 0]]
+       [0, [950, 830, 0], Easing.outBack],
+       [0.25, [950, 830, 0], Easing.outBack],
+       [0.55, [950, 680, 0]]
+     ]),
+     scale: $timeline([
+       [0, [0.2, 0.2], Easing.outBack],
+       [0.25, [0.2, 0.2], Easing.outBack],
+       [0.55, [1, 1]]
+     ]),
+     opacity: $timeline([
+       [0, 0],
+       [0.25, 0],
+       [0.55, 1]
      ])
    }, 
    image: {
      translate: $timeline([
-       [0, [760, 1010, 0], Easing.outBack],
-       [0.2, [760, 310, 0]]
+       [0, [750, 600, 0], Easing.outBack],
+       [0.3, [750, 600, 0], Easing.outBack],
+       [0.6, [750, 310, 0]]
      ]),
      opacity: $timeline([
-       [0, 0, Easing.outCubic],
-       [0.3, 1]
+       [0, 0],
+       [0.3, 0],
+       [0.6, 1]
      ])
    }, 
   };
@@ -91,18 +113,31 @@ angular.module('famous-angular')
   $scope.modules = {
    text: {
      translate: $timeline([
-       [0, [1430, 630, 0], Easing.inOutQuart],
-       [0.2, [1430, 630, 0]]
+       [0, [1510, 830, 0], Easing.outBack],
+       [0.35, [1510, 830, 0], Easing.outBack],
+       [0.65, [1510, 680, 0]]
+     ]),
+     scale: $timeline([
+       [0, [0.2, 0.2], Easing.outBack],
+       [0.35, [0.2, 0.2], Easing.outBack],
+       [0.65, [1, 1]]
+     ]),
+     opacity: $timeline([
+       [0, 0],
+       [0.35, 0],
+       [0.65, 1]
      ])
    }, 
    image: {
      translate: $timeline([
-       [0, [1320, 930, 0], Easing.outBack],
-       [0.2, [1320, 330, 0]]
+       [0, [1320, 630, 0], Easing.outBack],
+       [0.4, [1320, 630, 0], Easing.outBack],
+       [0.7, [1320, 330, 0]]
      ]),
      opacity: $timeline([
-       [0, 0, Easing.outCubic],
-       [0.3, 1]
+       [0, 0],
+       [0.4, 0],
+       [0.7, 1]
      ])
    }, 
   };
