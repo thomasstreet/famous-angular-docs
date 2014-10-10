079c0e2c59ea68d400accae246b3395a8c38c367
Andre Levi <andrelevi09@gmail.com>
2014-10-09 20:31:25 -0700

    determine the forced resolution based upon media query

--- app/viewport-scale.js
+++ app/viewport-scale.js
@@ -1,77 +1,90 @@
 angular.module('famous-angular')
 
-.run(function($rootScope) {
-    var NAVBAR = {
-      HEIGHT: 100
-    };
+.run(function($rootScope, $media) {
 
+  var NAVBAR = {
+    HEIGHT: 100
+  };
+
+  var _forcedResolution;
+
+  determineResolution();
+  setLeftOffset();
+
+  $(window).bind('resize', function() {
+    determineResolution();
+    setLeftOffset();
+  });
+
+  function determineResolution() {
     // These dimensions will always be set on the fa-app via CSS, regardless 
     // of the user's window dimensions
-    //var FORCED_RESOLUTION = {
-      //WIDTH: 1920,
-      //HEIGHT: 1080 - NAVBAR.HEIGHT
-    //};
+    if ($media.$query('sm')) {
+      _forcedResolution = {
+        width: 1920,
+        height: 1080 - NAVBAR.HEIGHT
+      };
+    } else {
+      _forcedResolution = {
+        width: 414,
+        height: 736 - NAVBAR.HEIGHT
+      };
+    }
+  }
 
-    var FORCED_RESOLUTION = {
-      WIDTH: 414,
-      HEIGHT: 736 - NAVBAR.HEIGHT
+  // Set the left offset based upon the scale amount, to re-center the fa-app
+  function setLeftOffset() {
+    var scale = getViewportScale();
+
+    var leftOffset = (window.innerWidth / 2) - (scale * _forcedResolution.width / 2);
+  
+    $('#fa-app').css({
+      '-webkit-transform' : 'scale(' + scale + ', ' + scale + ')',
+      '-moz-transform'    : 'scale(' + scale + ', ' + scale + ')',
+      '-ms-transform'     : 'scale(' + scale + ', ' + scale + ')',
+      '-o-transform'      : 'scale(' + scale + ', ' + scale + ')',
+      'transform'         : 'scale(' + scale + ', ' + scale + ')' 
+    });
+    $('#fa-app').css({
+      '-webkit-transform-origin' : '0 0',
+      '-moz-transform-origin'    : '0 0',
+      '-ms-transform-origin'     : '0 0',
+      '-o-transform-origin'      : '0 0',
+      'transform-origin'         : '0 0' 
+    });
+
+    // Don't set a left value if the window width is greater than the
+    // supported resolution.  Instead, let margin: auto center the viewport
+    if (window.innerWidth > _forcedResolution.width) return;
+
+    $('#fa-app').css('left', Math.floor(leftOffset));
+  }
+
+  function getViewportScale() {
+    var xScale, yScale;
+
+    var viewport = {
+      height: window.innerHeight - NAVBAR.HEIGHT,
+      width: window.innerWidth
     };
 
-    setLeftOffset();
-    $(window).bind('resize', setLeftOffset);
-
-    // Set the left offset based upon the scale amount, to re-center the fa-app
-    function setLeftOffset() {
-      var scale = getViewportScale();
-
-      var leftOffset = (window.innerWidth / 2) - (scale * FORCED_RESOLUTION.WIDTH / 2);
-    
-      $('#fa-app').css({
-        '-webkit-transform' : 'scale(' + scale + ', ' + scale + ')',
-        '-moz-transform'    : 'scale(' + scale + ', ' + scale + ')',
-        '-ms-transform'     : 'scale(' + scale + ', ' + scale + ')',
-        '-o-transform'      : 'scale(' + scale + ', ' + scale + ')',
-        'transform'         : 'scale(' + scale + ', ' + scale + ')' 
-      });
-      $('#fa-app').css({
-        '-webkit-transform-origin' : '0 0',
-        '-moz-transform-origin'    : '0 0',
-        '-ms-transform-origin'     : '0 0',
-        '-o-transform-origin'      : '0 0',
-        'transform-origin'         : '0 0' 
-      });
-
-      // Don't set a left value if the window width is greater than the
-      // supported resolution.  Instead, let margin: auto center the viewport
-      if (window.innerWidth > FORCED_RESOLUTION.WIDTH) return;
-
-      $('#fa-app').css('left', Math.floor(leftOffset));
+    if (viewport.height < _forcedResolution.height) {
+      yScale = viewport.height / _forcedResolution.height;
+      yScale = yScale.toFixed(2);
+    } else {
+      yScale = 1;
     }
 
-    function getViewportScale() {
-      var xScale, yScale;
+    if (viewport.width < _forcedResolution.width) {
+      xScale = viewport.width / _forcedResolution.width;
+      xScale = xScale.toFixed(2);
+    } else {
+      xScale = 1;
+    }
 
-      var viewport = {
-        height: window.innerHeight - NAVBAR.HEIGHT,
-        width: window.innerWidth
-      };
+    var smallestScale = xScale < yScale ? xScale : yScale;
 
-      if (viewport.height < FORCED_RESOLUTION.HEIGHT) {
-        yScale = viewport.height / FORCED_RESOLUTION.HEIGHT;
-        yScale = yScale.toFixed(2);
-      } else {
-        yScale = 1;
-      }
+    return smallestScale;
+  };
 
-      if (viewport.width < FORCED_RESOLUTION.WIDTH) {
-        xScale = viewport.width / FORCED_RESOLUTION.WIDTH;
-        xScale = xScale.toFixed(2);
-      } else {
-        xScale = 1;
-      }
-
-      var smallestScale = xScale < yScale ? xScale : yScale;
-
-      return smallestScale;
-    };
 });
