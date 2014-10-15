add0a9de0ab004a1d42eb55563eadf5ecc7d1803
Andre Levi <andrelevi09@gmail.com>
2014-10-15 12:33:06 -0700

    set up basic mobile nav carousel

--- app/state-mobile-nav-dir.js
+++ app/state-mobile-nav-dir.js
@@ -26,12 +26,98 @@ angular.module('famous-angular')
 
 /*--------------------------------------------------------------*/
 
-  $scope.navbar = {
-    opacity: $timeline([
-      [0, 0],
-      [1, 1]
-    ])
+  $(window).bind('scroll', function() {
+    _pointer.halt();
+    var t = getTimelineFromScroll();
+    _pointer.set(t, {duration: 300});
+  });
+
+  function getTimelineFromScroll() {
+    var scrollMax = stateScrollUtils.scrollMax();
+    var stateCount = stateScrollUtils.stateCount();
+    var halfOfScrollRange = scrollMax / stateCount / 2;
+
+    var scaleScroll = $timeline([
+      [halfOfScrollRange, 0],
+      [scrollMax - halfOfScrollRange, 6]
+    ]);
+
+    var scrollPosition = window.pageYOffset;
+    return scaleScroll(scrollPosition);
+  }
+
+
+  var _pointer = new Transitionable(0);
+  window.setPointer = function(num) {
+    _pointer.set(num, {duration: 300});
   };
+
+  $scope.itemSize = [340, 60];
+
+  $scope.getTranslate = function($index) {
+    var position = $index - _pointer.get();
+    var xTranslate = position * ($scope.itemSize[0] - 0);
+    return [xTranslate, 0, 0];
+  };
+
+  $scope.getRotateY = function($index) {
+    var position = $index - _pointer.get();
+    return (6.28 / 7) * position;
+  };
+
+  $scope.getOpacity = function($index) {
+    var position = $index - _pointer.get();
+    position = Math.abs(position);
+
+    return 1 - position + 0.5;
+  };
+
+  $scope.getOrigin = function($index) {
+    return [0.5, 0.5];
+
+
+    var position = $index - _pointer.get();
+    var xOrigin = 0.5;
+
+    var cappedPosition = Math.max(Math.min(position, 1), -1);
+
+    xOrigin += -cappedPosition * 0.5;
+
+    console.log('index', $index, 'xOrigin', xOrigin);
+
+    return [xOrigin, 0.5];
+  };
+
+  $scope.menuItems = [
+    {
+      text: 'Intro',
+      color: 'blue'
+    },
+    {
+      text: 'Render Tree',
+      color: 'red'
+    },
+    {
+      text: 'Data Binding',
+      color: 'blue'
+    },
+    {
+      text: 'Angular Directives',
+      color: 'red'
+    },
+    {
+      text: 'Organization',
+      color: 'blue'
+    },
+    {
+      text: 'No Compromises',
+      color: 'red'
+    },
+    {
+      text: 'Download',
+      color: 'blue'
+    }
+  ]
 
 /*--------------------------------------------------------------*/
 
