46df92d8809336bfb5961bda03d8b22b69cf19d5
Andre Levi <andrelevi09@gmail.com>
2014-10-16 19:50:16 -0700

    re-organize mobile nav code

--- app/state-mobile-nav-dir.js
+++ app/state-mobile-nav-dir.js
@@ -20,105 +20,6 @@ angular.module('famous-angular')
 
 /*--------------------------------------------------------------*/
 
-  $scope.t = new Transitionable(0);
-
-  $(window).bind('scroll', function() {
-    $scope.t.halt();
-    var t = getTimelineFromScroll();
-    $scope.t.set(t, {duration: 0});
-  });
-
-  function getTimelineFromScroll() {
-    var scrollMax = stateScrollUtils.scrollMax();
-    var stateCount = stateScrollUtils.stateCount();
-    var halfOfScrollRange = scrollMax / stateCount / 2;
-
-    var scaleScroll = $timeline([
-      [halfOfScrollRange, 0],
-      [scrollMax - halfOfScrollRange, 6]
-    ]);
-
-    var scrollPosition = window.pageYOffset;
-    return scaleScroll(scrollPosition);
-  }
-
-  window.setPointer = function(num) {
-    $scope.t.set(num, {duration: 1600, curve: Easing.outElastic});
-  };
-
-  var isGridMode = false;
-  var gridModeTran = new Transitionable(0);
-  $scope.gridModeT = gridModeTran;
-
-  $scope.toggleGridMode = function(state) {
-    isGridMode = !isGridMode;
-
-    if (isGridMode) {
-      gridModeTran.set(1, { duration: 350 });
-    } else {
-      gridModeTran.set(0, { duration: 350 }, function() {
-        $state.go(state);
-      });
-    }
-  }
-
-  $scope.isCurrentState = function(state) {
-    return $state.current.name === state;
-  };
-
-  $scope.rotateX = function($index, timeValue) {
-    if (gridModeTran.get()) return 0;
-
-    return $timeline([
-      [$index, -Math.PI],
-      [$index + 1, 0],
-      [$index + 2, Math.PI]
-    ])(timeValue);
-  };
-
-  $scope.card = {
-    scale: $timeline([
-      [0, [0.75, 0.75], Easing.inQuad],
-      [0.5, [1, 1]]
-    ])
-  };
-
-  $scope.gridModePosition = function($index) {
-    return $timeline([
-      [0, [0, 0, 0], Easing.outBack],
-      [0.8, [0, $index * 150 + 120, 0]],
-    ])(gridModeTran.get());
-  };
-
-  // Resizing surfaces doesn't work that well, so translate the overlay
-  // off screen when not in use, to prevent it from gobbling up events.
-  $scope.overlay = {
-    translate: function() {
-      return $timeline([
-        [0, [0, -1366, -10]],
-        // Translate z-value should be lower than the nav, but above all other
-        // content, so that only the nav is unaffected
-        [0.01, [0, 0, 9]]
-      ])(gridModeTran.get());
-    },
-    opacity: function() {
-      return $timeline([
-        [0, 0, Easing.outCubic],
-        [1, 0.85]
-      ])(gridModeTran.get());
-    },
-    x: {
-      translate: function() {
-        return $timeline([
-          [0.1, [360, 1090, 1000], Easing.outQuad],
-          // Translate z-value should be lower than the nav, but above all other
-          // content, so that only the nav is unaffected
-          [0.5, [360, 1090, 10]]
-        ])(gridModeTran.get());
-      }
-    }
-  };
-
   $scope.menuItems = [
     {
       text: 'Render Tree',
@@ -145,5 +46,105 @@ angular.module('famous-angular')
       state: 'download'
     }
   ];
+
+/*--------------------------------------------------------------*/
+
+  $scope.t = new Transitionable(0);
+
+  $(window).bind('scroll', function() {
+    $scope.t.halt();
+    var t = getTimelineFromScroll();
+    $scope.t.set(t, {duration: 0});
+  });
+
+  function getTimelineFromScroll() {
+    var scrollMax = stateScrollUtils.scrollMax();
+    var stateCount = stateScrollUtils.stateCount();
+    var halfOfScrollRange = scrollMax / stateCount / 2;
+
+    var scaleScroll = $timeline([
+      [halfOfScrollRange, 0],
+      [scrollMax - halfOfScrollRange, 6]
+    ]);
+
+    var scrollPosition = window.pageYOffset;
+    return scaleScroll(scrollPosition);
+  }
+
+  window.setPointer = function(num) {
+    $scope.t.set(num, {duration: 1600, curve: Easing.outElastic});
+  };
+
+/*--------------------------------------------------------------*/
+
+  var isGridMode = false;
+  var gridModeTran = new Transitionable(0);
+  $scope.gridModeT = gridModeTran;
+
+  $scope.gridMode = {
+    toggle: function(state) {
+      isGridMode = !isGridMode;
+
+      if (isGridMode) {
+        gridModeTran.set(1, { duration: 350 });
+      } else {
+        gridModeTran.set(0, { duration: 350 }, function() {
+          if (state) $state.go(state);
+        });
+      }
+    },
+    translate: function($index, gridModeTimeValue) {
+      return $timeline([
+        [0, [0, 0, 0], Easing.outBack],
+        [0.8, [0, $index * 150 + 120, 0]],
+      ])(gridModeTimeValue);
+    }
+  };
+
+  $scope.tile = {
+    scale: $timeline([
+      [0, [0.75, 0.75], Easing.inQuad],
+      [0.5, [1, 1]]
+    ]),
+    rotateX: function($index, timeValue) {
+      if (gridModeTran.get()) return 0;
+
+      return $timeline([
+        [$index, -Math.PI],
+        [$index + 1, 0],
+        [$index + 2, Math.PI]
+      ])(timeValue);
+    },
+    isCurrentState: function(state) {
+      return $state.current.name === state;
+    }
+  };
+
+
+/*--------------------------------------------------------------*/
+
+  // Resizing surfaces doesn't work that well, so translate the overlay
+  // off screen when not in use, to prevent it from gobbling up events.
+  $scope.overlay = {
+    translate: $timeline([
+      [0, [0, -1366, -10]],
+      // Translate z-value should be lower than the nav, but above all other
+      // content, so that only the nav is unaffected
+      [0.01, [0, 0, 9]]
+    ]),
+    opacity: $timeline([
+      [0, 0, Easing.outCubic],
+      [1, 0.85]
+    ]),
+
+    xIcon: {
+      translate: $timeline([
+        [0.1, [360, 1090, 1000], Easing.outQuad],
+        // Translate z-value should be lower than the nav, but above all other
+        // content, so that only the nav is unaffected
+        [0.5, [360, 1090, 10]]
+      ])
+    }
+  };
 
 });
