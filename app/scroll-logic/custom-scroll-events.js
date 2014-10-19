90e6ad51fa06b1ddd2ad981bae50bfe79829c2bc
Andre Levi <andrelevi09@gmail.com>
2014-10-19 12:10:34 -0700

    create custom-scroll-events.js module.  get rid of jquery scrollend/scrollstart library

---
+++ app/scroll-logic/custom-scroll-events.js
@@ -0,0 +1,47 @@
+angular.module('famous-angular')
+
+.run(function($rootScope) {
+  /* Custom events to listen for:
+
+    $rootScope.$on('fa-scrollend', function() {
+      console.log('scrollend!');
+    });
+
+    $rootScope.$on('fa-scrollstart', function() {
+      console.log('scrollstart!');
+    });
+
+    $rootScope.$on('fa-scroll', function() {
+      console.log('scroll!');
+    });
+
+ */
+
+  var $scrolledElement = $('#scroll-container');
+  var DELAY_UNTIL_SCROLLEND = 300;
+
+  var scrollendTimeout;
+
+  $scrolledElement.bind('scroll', function() {
+
+    // If a scrollendTimeout exists, this scroll event is in the middle 
+    // of a scroll session 
+    if (scrollendTimeout) {
+      clearTimeout(scrollendTimeout);
+      scrollendTimeout = null;
+
+    // If there isn't a pending scrollend timeout, this is a fresh scroll session
+    } else {
+      $rootScope.$emit('fa-scrollstart');
+    }
+
+    $rootScope.$emit('fa-scroll');
+
+    scrollendTimeout = setTimeout(function() {
+      $rootScope.$emit('fa-scrollend');
+      scrollendTimeout = null;
+    }, DELAY_UNTIL_SCROLLEND)
+
+  });
+
+});
