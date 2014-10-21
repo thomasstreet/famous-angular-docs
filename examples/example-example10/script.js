a643ac6893718a80cbebaea20c8c9d03064124f6
Zack Brown <z@charybrown.com>
2014-10-21 14:37:44 +0200

    chore: update docs build

--- examples/example-example10/script.js
+++ examples/example-example10/script.js
@@ -4,5 +4,5 @@ angular.module('faInputExampleApp', ['famous.angular'])
       $scope.myClickHandler = function($event) {
         console.log($event);
         $scope.clicked++;
-      }; 
+      };
   }]);
---
+++ examples/example-example53/script.js
@@ -0,0 +1,3 @@
+angular.module('faSurfaceExampleApp', ['famous.angular'])
+      .controller('SurfaceCtrl', ['$scope', '$famous', function($scope, $famous) {
+    }]);
