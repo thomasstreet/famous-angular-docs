7783817129d69b09a6527fdc3b59fd1e3c33799f
Andre Levi <andrelevi09@gmail.com>
2014-10-22 09:55:11 -0700

    disable arrow key state triggered state changes when typing in a text box

--- app/scroll-logic/keydown-state-changer.js
+++ app/scroll-logic/keydown-state-changer.js
@@ -3,6 +3,10 @@ angular.module('famous-angular')
 .run(function($state, stateUtils) {
 
   window.addEventListener('keydown', function(e) {
+    // If typing in a text box, allow for arrow key movement without triggering
+    // state change
+    if (e.target.tagName.toLowerCase() === 'input') return;
+
     var key = e.keyCode;
 
     // If key is left, go back a state
